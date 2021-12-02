a932b8e5619c01774a46c9e6384f467b0fd0edb0
Ubuntu <ubuntu@ip-172-31-12-237.us-east-2.compute.internal>
2021-12-02 06:25:39 +0000

    added Category model test - test_category.rb

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @category = Category.new(name: 'Cha')
+  end
+  
+  test "should be valid" do
+    assert @category.valid?
+  end
+  
+  test "name should be present" do
+    @category.name = ''
+    assert_not @category.valid?
+  end
 end
